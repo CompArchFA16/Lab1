e002a8a19d78851a9a74e7fb8ce6cdd4a0b6e42f
abuchele <anna.buchele@students.olin.edu>
2016-10-02 15:59:31 -0400

    updated vcd

--- alu.t.v
+++ alu.t.v
@@ -1,7 +1,7 @@
 `timescale 1 ns / 1 ps
 `include "alu.v"
 
-module testALU();
+/*module testALU();
     reg[31:0] operandA, operandB;
     reg[2:0] command;
     wire[31:0] result;
@@ -16,13 +16,121 @@ module testALU();
 
     $display("   operandA   operandB   |  command   ||  zero   carryout   overflow  | result ||   Exp zero   Exp carryout   Exp overflow   |   Exp result");
     
-    $display("Test Basic Addition:");
+    $display("Adder Test:");
 
     // this is our test bench
-    operandA=32'b11;operandB=32'b1000000000000000000000000000011; #1000
+    operandA=32'b11;operandB=32'b101; #1000
     $display(" %b %b |  %b      ||      %b    %b    %b    %b   |     %b     ||     0     0     0     |   0 ", operandA, operandB, command, zero, carryout, overflow, result);
 
+    $display("Control Test:");
+    operandA=32'b11;operandB=32'b101; #1000
+    $display(" %b %b |  %b      ||      %b    %b    %b    %b   |     %b     ||     0     0     0     |   0 ", operandA, operandB, command, zero, carryout, overflow, result);
+    end
+endmodule*/
+
+
+module testadd();
+    reg[31:0] a, b;
+    wire[31:0] result;
+
+
+    adder32 myALU (result, a, b);
+
+    initial begin
+    //dump to vcd file so we can look at waveform
+    $dumpfile("alu.vcd");
+    $dumpvars(0, testadd);
+
+
+    
+    $display("Adder Test:");
+    $display("     a          b     |   result   ||   Exp result");
+
+    a='h0;b='h0; #1000
+    $display(" %h   %h  |  %h  ||    00000000", a, b, result);
+    a='h0;b='h1; #1000
+    $display(" %h   %h  |  %h  ||    00000001", a, b, result);
+    a='h1;b='h1; #1000
+    $display(" %h   %h  |  %h  ||    00000002", a, b, result);
+    a='h1100010;b='h0101010; #1000
+    $display(" %h   %h  |  %h  ||    01201020", a, b, result);
+    a='h11111111;b='h1; #1000
+    $display(" %h   %h  |  %h  ||    11111112", a, b, result);
+    a='h92946327;b='h90997372; #1000
+    $display(" %h   %h  |  %h  ||    9196eb4c", a, b, result);
 
 
     end
 endmodule
+
+/*module testsubtract();
+    reg[31:0] a, b;
+    wire[31:0] result;
+
+
+    subtract32 myALU (result, a, b);
+
+    initial begin
+    //dump to vcd file so we can look at waveform
+    $dumpfile("sub.vcd");
+    $dumpvars(0, testsubtract);
+
+
+    
+    $display("Subtract Test:");
+    $display("     a          b     |   result   ||   Exp result");
+
+    a='h111;b='h101; #1000
+    $display(" %h    %h |  %h  ", a, b, result);
+
+
+    end
+endmodule*/
+
+/*module testsubtract();
+    reg[31:0] a, b;
+    wire[31:0] result;
+
+
+    subtract32 myALU (result, a, b);
+
+    initial begin
+    //dump to vcd file so we can look at waveform
+    $dumpfile("sub.vcd");
+    $dumpvars(0, testsubtract);
+
+
+    
+    $display("Subtract Test:");
+    $display("     a          b     |   result   ||   Exp result");
+
+    a='h111;b='h101; #1000
+    $display(" %h    %h |  %h  ", a, b, result);
+
+
+    end
+endmodule*/
+
+/*module testxor();
+    reg[31:0] a, b;
+    wire[31:0] result;
+
+
+    xor32 myALU (result, a, b);
+
+    initial begin
+    //dump to vcd file so we can look at waveform
+    $dumpfile("xor.vcd");
+    $dumpvars(0, testxor);
+
+
+    
+    $display("Subtract Test:");
+    $display("     a          b     |   result   ||   Exp result");
+
+    a='h111;b='h101; #1000
+    $display(" %h    %h |  %h  ", a, b, result);
+
+
+    end
+endmodule*/
--- alu.v
+++ alu.v
@@ -7,18 +7,48 @@ output          overflow,
 input[31:0]     operandA,
 input[31:0]     operandB,
 input[2:0]      command
+
 );
     
 endmodule
 
-module adder
-(
+module adder32(
+   // Outputs
+   result, 
+   // Inputs
+   a, b
+   );
+   input[31:0] a;
+   input[31:0] b;
+   output [31:0] result;
+   wire [32:0] sum = {1'b0,a} + {1'b0,b};
+   assign      result = sum[32] ? sum[32:1]: sum[31:0];
+endmodule // adder32
 
+module subtract32(
+   // Outputs
+   result, 
+   // Inputs
+   a, b
+   );
+   input[31:0] a;
+   input[31:0] b;
+   output [31:0] result;
+   wire [32:0] sum = {1'b0,a} - {1'b0,b};
+   assign      result = sum[32] ? sum[32:1]: sum[31:0];
+endmodule // adder32
 
-
-
-
-
-	);
-
-endmodule
+/*module xor32(
+   // Outputs
+   result, 
+   // Inputs
+   a, b
+   );
+   input[31:0] a;
+   input[31:0] b;
+   output [31:0] result;
+   wire [32:0] sum;
+   xor xorgate(sum, {1'b0,a}, {1'b0,b})
+   assign      result = sum[32] ? sum[32:1]: sum[31:0];
+endmodule // adder32
+*/
