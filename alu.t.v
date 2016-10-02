28ec458a7bb868d6c051e0f18798f127dfdcd6c2
abuchele <anna.buchele@students.olin.edu>
2016-10-02 17:51:09 -0400

    Add files via upload

--- alu.t.v
+++ alu.t.v
@@ -1,5 +1,6 @@
 `timescale 1 ns / 1 ps
 `include "alu.v"
+/*include "genvartest.v"*/
 
 /*module testALU();
     reg[31:0] operandA, operandB;
@@ -29,7 +30,7 @@
 endmodule*/
 
 
-module testadd();
+/*module testadd();
     reg[31:0] a, b;
     wire[31:0] result;
 
@@ -60,6 +61,215 @@ module testadd();
     $display(" %h   %h  |  %h  ||    9196eb4c", a, b, result);
 
 
+    end
+endmodule*/
+
+/*'''Completed, working'''*/
+/*module testand();
+    wire carryout;
+    wire overflow;
+    wire[31:0] andResult;
+    reg [31:0] operandA;
+    reg [31:0] operandB;
+
+
+    and32 myALU (carryout, overflow, andResult, operandA, operandB);
+
+    initial begin
+    //dump to vcd file so we can look at waveform
+    $dumpfile("alu.vcd");
+    $dumpvars(0, testand);
+    $display(" operandA   operandB  |  result  ||   Exp result");
+
+    operandA='h0;operandB='h0; #1000
+    $display(" %h   %h  | %h ||    00000000", operandA,operandB,  andResult);
+    operandA='h181;operandB='h263; #1000
+    $display(" %h   %h  | %h ||    00000001", operandA,operandB,  andResult);
+    operandA='h161;operandB='h161; #1000
+    $display(" %h   %h  | %h ||    00000161", operandA,operandB,  andResult);
+    operandA='hffffffff;operandB='hf00f001; #1000
+    $display(" %h   %h  | %h ||    0f00f001", operandA,operandB,  andResult);
+    operandA='hffffffff;operandB='hcccccccc; #1000
+    $display(" %h   %h  | %h ||    cccccccc", operandA,operandB,  andResult);
+    end
+endmodule*/
+/*
+module testor();
+    wire carryout;
+    wire overflow;
+    wire[31:0] andResult;
+    reg [31:0] operandA;
+    reg [31:0] operandB;
+
+
+    or32 myALU (carryout, overflow, andResult, operandA, operandB);
+
+    initial begin
+    //dump to vcd file so we can look at waveform
+    $dumpfile("alu.vcd");
+    $dumpvars(0, testor);
+    $display(" operandA   operandB  |  result  ||   Exp result");
+
+    operandA='h0;operandB='h0; #1000
+    $display(" %h   %h  | %h ||    00000000", operandA,operandB,  andResult);
+    operandA='h181;operandB='h263; #1000
+    $display(" %h   %h  | %h ||    000003e3", operandA,operandB,  andResult);
+    operandA='h161;operandB='h161; #1000
+    $display(" %h   %h  | %h ||    00000161", operandA,operandB,  andResult);
+    operandA='hffffffff;operandB='hf00f001; #1000
+    $display(" %h   %h  | %h ||    ffffffff", operandA,operandB,  andResult);
+    operandA='hffffffff;operandB='hcccccccc; #1000
+    $display(" %h   %h  | %h ||    ffffffff", operandA,operandB,  andResult);
+    end
+endmodule*/
+
+/*
+module testnor();
+    wire carryout;
+    wire overflow;
+    wire[31:0] andResult;
+    reg [31:0] operandA;
+    reg [31:0] operandB;
+
+
+    nor32 myALU (carryout, overflow, andResult, operandA, operandB);
+
+    initial begin
+    //dump to vcd file so we can look at waveform
+    $dumpfile("alu.vcd");
+    $dumpvars(0, testnor);
+    $display(" operandA   operandB  |  result  ||   Exp result");
+
+    operandA='h0;operandB='h0; #1000
+    $display(" %h   %h  | %h ||    ffffffff", operandA,operandB,  andResult);
+    operandA='h181;operandB='h263; #1000
+    $display(" %h   %h  | %h ||    fffffc1c", operandA,operandB,  andResult);
+    operandA='h161;operandB='h161; #1000
+    $display(" %h   %h  | %h ||    fffffe9e", operandA,operandB,  andResult);
+    operandA='hffffffff;operandB='hf00f001; #1000
+    $display(" %h   %h  | %h ||    00000000", operandA,operandB,  andResult);
+    operandA='hffffffff;operandB='hcccccccc; #1000
+    $display(" %h   %h  | %h ||    00000000", operandA,operandB,  andResult);
+    end
+endmodule*/
+
+
+/*
+module testnand();
+    wire carryout;
+    wire overflow;
+    wire[31:0] andResult;
+    reg [31:0] operandA;
+    reg [31:0] operandB;
+
+
+    nand32 myALU (carryout, overflow, andResult, operandA, operandB);
+
+    initial begin
+    //dump to vcd file so we can look at waveform
+    $dumpfile("alu.vcd");
+    $dumpvars(0, testnand);
+    $display(" operandA   operandB  |  result  ||   Exp result");
+
+    operandA='h0;operandB='h0; #1000
+    $display(" %h   %h  | %h ||    ffffffff", operandA,operandB,  andResult);
+    operandA='h181;operandB='h263; #1000
+    $display(" %h   %h  | %h ||    fffffffe", operandA,operandB,  andResult);
+    operandA='h161;operandB='h161; #1000
+    $display(" %h   %h  | %h ||    fffffe9e", operandA,operandB,  andResult);
+    operandA='hffffffff;operandB='hf00f001; #1000
+    $display(" %h   %h  | %h ||    f0ff0ffe", operandA,operandB,  andResult);
+    operandA='hffffffff;operandB='hcccccccc; #1000
+    $display(" %h   %h  | %h ||    33333333", operandA,operandB,  andResult);
+    end
+endmodule*/
+/*
+module testadd();
+    wire carryout;
+    wire overflow;
+    wire[31:0] andResult;
+    reg [31:0] operandA;
+    reg [31:0] operandB;
+
+    add32 myALU (carryout, overflow, andResult, operandA, operandB);
+
+    initial begin
+    //dump to vcd file so we can look at waveform
+    $dumpfile("alu.vcd");
+    $dumpvars(0, testadd);
+    $display("     a          b     | overflow   carryout |  result  ||   Exp result");
+
+    operandA='h0;operandB='h0; #1000
+    $display(" %h   %h  |     %h         %h     | %h ||    00000000", operandA,operandB,  overflow, carryout, andResult);
+    operandA='h181;operandB='h263; #1000
+    $display(" %h   %h  |     %h         %h     | %h ||    000003e4", operandA,operandB,  overflow, carryout, andResult);
+    operandA='h161;operandB='h161; #1000
+    $display(" %h   %h  |     %h         %h     | %h ||    000002c2", operandA,operandB,  overflow, carryout, andResult);
+    operandA='hffffffff;operandB='hf00f001; #1000
+    $display(" %h   %h  |     %h         %h     | %h ||    0f00f000", operandA,operandB,  overflow, carryout, andResult);
+    operandA='hffffffff;operandB='hcccccccc; #1000
+    $display(" %h   %h  |     %h         %h     | %h ||    cccccccb", operandA,operandB,  overflow, carryout, andResult);
+    operandA='hffffffff;operandB='hffffffff; #1000
+    $display(" %h   %h  |     %h         %h     | %h ||    fffffffe", operandA,operandB,  overflow, carryout, andResult);
+    operandA='hc0000000;operandB='h80000000; #1000
+    $display(" %h   %h  |     %h         %h     | %h ||    40000000", operandA,operandB,  overflow, carryout, andResult);
+    end
+endmodule
+*/
+/*
+module testsubtract();
+    wire carryout;
+    wire overflow;
+    wire[31:0] andResult;
+    reg [31:0] operandA;
+    reg [31:0] operandB;
+    reg M;
+
+    add32 myALU (carryout, overflow, andResult, operandA, operandB, M);
+
+    initial begin
+    //dump to vcd file so we can look at waveform
+    $dumpfile("alu.vcd");
+    $dumpvars(0, testsubtract);
+    $display("     a          b      M  | overflow   carryout |  result  ||   Exp result");
+
+    operandA='h01;operandB='h0;M=1; #1000
+    $display(" %h   %h   %h  |     %h         %h     | %h ||    00000000", operandA,operandB, M, overflow, carryout, andResult);
+
+    end
+endmodule
+*/
+
+
+
+/*'''test skeleton'''*/
+module testadd();
+    wire carryout;
+    wire overflow;
+    wire[31:0] andResult;
+    reg [31:0] operandA;
+    reg [31:0] operandB;
+    reg M;
+
+
+    add32 myALU (carryout, overflow, andResult, operandA, operandB, M);
+
+    initial begin
+    //dump to vcd file so we can look at waveform
+    $dumpfile("alu.vcd");
+    $dumpvars(0, testadd);
+    $display("     a          b     | overflow   carryout |  result  ||   Exp result");
+
+    operandA='h11;operandB='h1;M=1; #1000
+    $display(" %h   %h  |     %h         %h     | %h ||    00000000", operandA,operandB,  overflow, carryout, andResult);
+    /*operandA='h181;operandB='h263; #1000
+    $display(" %h   %h  |     %h         %h     | %h ||    00000001", operandA,operandB,  overflow, carryout, andResult);
+    operandA='h161;operandB='h161; #1000
+    $display(" %h   %h  |     %h         %h     | %h ||    00000161", operandA,operandB,  overflow, carryout, andResult);
+    operandA='hffffffff;operandB='hf00f001; #1000
+    $display(" %h   %h  |     %h         %h     | %h ||    0f00f001", operandA,operandB,  overflow, carryout, andResult);
+    operandA='hffffffff;operandB='hcccccccc; #1000
+    $display(" %h   %h  |     %h         %h     | %h ||    cccccccc", operandA,operandB,  overflow, carryout, andResult);*/
     end
 endmodule
 
@@ -132,5 +342,36 @@ endmodule*/
     $display(" %h    %h |  %h  ", a, b, result);
 
 
+    end
+endmodule*/
+
+/*module testFullAdder() ;
+    reg a, b, carryin, M;
+    wire sum, carryout;
+
+    structuralFullAdder adder (sum, carryout, a, b, carryin);
+
+
+    initial begin
+    $dumpfile("adder.vcd");
+    $dumpvars(0, testFullAdder);
+
+    $display("A B     M   | Sum Carryout | Expected Output");
+    a=1;b=0;carryin=0;M=1; #1000
+    $display("%b %b    %b    |    %b     %b   | 1  0", a, b, M, sum, carryout);
+    a=1;b=1;carryin=0;M=1; #1000
+    $display("%b %b    %b    |    %b     %b   | 0  0", a, b, M, sum, carryout);
+    a=0;b=1;carryin=0; #1000
+    $display("%b %b    %b    |    %b     %b   | 1  0", a, b, carryin, sum, carryout);
+    a=0;b=1;carryin=1; #1000
+    $display("%b %b    %b    |    %b     %b   | 0  1", a, b, carryin, sum, carryout);
+    a=1;b=0;carryin=0; #1000
+    $display("%b %b    %b    |    %b     %b   | 1  0", a, b, carryin, sum, carryout);
+    a=1;b=0;carryin=1; #1000
+    $display("%b %b    %b    |    %b     %b   | 0  1", a, b, carryin, sum, carryout);
+    a=1;b=1;carryin=0; #1000
+    $display("%b %b    %b    |    %b     %b   | 0  1", a, b, carryin, sum, carryout);
+    a=1;b=1;carryin=1; #1000
+    $display("%b %b    %b    |    %b     %b   | 1  1", a, b, carryin, sum, carryout);
     end
 endmodule*/
--- alu.v
+++ alu.v
@@ -1,3 +1,42 @@
+
+`define NOT not #10 //fundamental, one input
+`define NAND nand #20 //fundamental, 2 input
+`define NOR nor #20 //fundamental, 2 input
+`define AND and #30 //made from inverter and NAND
+`define OR or #30 // made from inverter and OR
+`define XOR xor #60 //worst case three and gates 
+
+
+
+/*behavioral verilog done for test bench crafting/checking*/
+module behavioraladder32(
+   // Outputs
+   result, 
+   // Inputs
+   a, b
+   );
+   input[31:0] a;
+   input[31:0] b;
+   output [31:0] result;
+   wire [32:0] sum = {1'b0,a} + {1'b0,b};
+   assign      result = sum[32] ? sum[32:1]: sum[31:0];
+endmodule // adder32
+
+module behavioralsubtract32(
+   // Outputs
+   result, 
+   // Inputs
+   a, b
+   );
+   input[31:0] a;
+   input[31:0] b;
+   output [31:0] result;
+   wire [32:0] sum = {1'b0,a} - {1'b0,b};
+   assign      result = sum[32] ? sum[32:1]: sum[31:0];
+endmodule // adder32
+
+/*'''Our real code starts here'''*/
+
 module ALU
 (
 output[31:0]    result,
@@ -12,43 +51,126 @@ input[2:0]      command
     
 endmodule
 
-module adder32(
-   // Outputs
-   result, 
-   // Inputs
-   a, b
-   );
-   input[31:0] a;
-   input[31:0] b;
-   output [31:0] result;
-   wire [32:0] sum = {1'b0,a} + {1'b0,b};
-   assign      result = sum[32] ? sum[32:1]: sum[31:0];
-endmodule // adder32
 
-module subtract32(
-   // Outputs
-   result, 
-   // Inputs
-   a, b
-   );
-   input[31:0] a;
-   input[31:0] b;
-   output [31:0] result;
-   wire [32:0] sum = {1'b0,a} - {1'b0,b};
-   assign      result = sum[32] ? sum[32:1]: sum[31:0];
-endmodule // adder32
+module and32
+(
+output reg carryout=0,
+output reg overflow=0,
+output[31:0] andResult,
+input [31:0] operandA,
+input [31:0] operandB
+);
+genvar i;
+  generate
+  //ands all 32 bits
+    for (i = 0; i < 32; i = i+1)
+    begin : gen1
+      `AND(andResult[i],operandA[i],operandB[i]);
+    end
+  endgenerate
+endmodule
 
-/*module xor32(
-   // Outputs
-   result, 
-   // Inputs
-   a, b
-   );
-   input[31:0] a;
-   input[31:0] b;
-   output [31:0] result;
-   wire [32:0] sum;
-   xor xorgate(sum, {1'b0,a}, {1'b0,b})
-   assign      result = sum[32] ? sum[32:1]: sum[31:0];
-endmodule // adder32
-*/
+
+module or32
+(
+output reg carryout=0,
+output reg overflow=0,
+output[31:0] andResult,
+input [31:0] operandA,
+input [31:0] operandB
+);
+genvar i;
+  generate
+  //ands all 32 bits
+    for (i = 0; i < 32; i = i+1)
+    begin : gen1
+      `OR(andResult[i],operandA[i],operandB[i]);
+    end
+  endgenerate
+endmodule
+
+module nor32
+(
+output reg carryout=0,
+output reg overflow=0,
+output[31:0] andResult,
+input [31:0] operandA,
+input [31:0] operandB
+);
+genvar i;
+  generate
+  //ands all 32 bits
+    for (i = 0; i < 32; i = i+1)
+    begin : gen1
+      `NOR(andResult[i],operandA[i],operandB[i]);
+    end
+  endgenerate
+endmodule
+
+module nand32
+(
+output reg carryout=0,
+output reg overflow=0,
+output[31:0] andResult,
+input [31:0] operandA,
+input [31:0] operandB
+);
+genvar i;
+  generate
+  //ands all 32 bits
+    for (i = 0; i < 32; i = i+1)
+    begin : gen1
+      `NAND(andResult[i],operandA[i],operandB[i]);
+    end
+  endgenerate
+endmodule
+
+module structuralFullAdder
+(
+  // This is the unit module which the FullAdder4bit runs 4 times
+    output sum,
+    output carryout,
+    input a,
+    input b,
+    input carryin
+);
+wire AxorB, AxorBandcarryin, AandB;
+// XOR gate is true only if one of the two inputs is true, not both
+`XOR xorgate1(AxorB, a, b);
+`XOR xorgate2(sum, AxorB, carryin);
+// AND gate is true only if both inputs are true
+`AND andgate1(AxorBandcarryin, AxorB, carryin);
+`AND andgate2(AandB, a, b);
+/// OR gate is true if either or both of the inputs are true
+`OR orgate(carryout, AxorBandcarryin, AandB);
+endmodule
+
+module add32
+(
+output carryout,
+output overflow,
+output[31:0] andResult,
+input [31:0] operandA,
+input [31:0] operandB,
+input M
+);
+wire[31:0] carryoutin;
+wire[31:0] MxorB;
+`XOR xorgate0(MxorB, operandB, M);
+structuralFullAdder add0(andResult[0], carryoutin[0], operandA[0],MxorB[0], 0);  
+genvar i;
+  generate
+  //ands all 32 bits
+    for (i = 1; i < 31; i = i+1)
+    begin : gen1
+      /*`XOR xorgatei(MxorB[i], operandB[i], M);*/
+      `XOR xorgatei(MxorB, operandB[i], M);
+      structuralFullAdder addi(andResult[i], carryoutin[i], operandA[i],MxorB, carryoutin[i-1]);  
+    end
+  endgenerate
+`XOR xorgatei(MxorB, operandB[31], M);
+structuralFullAdder add31(andResult[31], carryout, operandA[31],MxorB, carryoutin[30]);  
+
+  `XOR overflowgate(overflow,carryoutin[30],carryout);
+endmodule
+
