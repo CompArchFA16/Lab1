4e072c49af79c7dd4ce245091181ec0bd71e2e8f
tj-kim <taejin.kim96@gmail.com>
2016-10-01 22:30:04 -0400

    slt update

--- alu.v
+++ alu.v
@@ -34,7 +34,7 @@ input[2:0]  ALUcommand
       `ADD:  begin muxindex = 0; invertB=0; othercontrolsignal = 0; end    
       `SUB:  begin muxindex = 0; invertB=1; othercontrolsignal = 0; end
       `XOR:  begin muxindex = 1; invertB=0; othercontrolsignal = 0; end    
-      `SLT:  begin muxindex = 2; invertB=0; othercontrolsignal = 0; end
+      `SLT:  begin muxindex = 2; invertB=1; othercontrolsignal = 0; end
       `AND:  begin muxindex = 3; invertB=0; othercontrolsignal = 1; end    
       `NAND: begin muxindex = 3; invertB=0; othercontrolsignal = 0; end
       `NOR:  begin muxindex = 4; invertB=0; othercontrolsignal = 0; end    
