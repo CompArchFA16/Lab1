bd7f52659d2c00488709394807b4d2b3835e27bf
David Zhu <hdavidzhu@gmail.com>
2016-10-03 11:39:10 -0400

    ADD: Multiplexer boilerplate.

---
+++ multiplexer_8_input.t.v
@@ -0,0 +1,20 @@
+`include "multiplexer_8_input.v"
+`timescale 1 ns / 1 ps
+
+module testMultiplexer8Input();
+
+  wire out;
+  reg[2:0] addresses;
+  reg[7:0] inputs;
+
+  Multiplexer8Input dut(out, addresses, inputs);
+
+  initial begin
+    $dumpfile("multiplexer_8_input.vcd");
+    $dumpvars;
+
+    $display("addresses inputs | out | EOut");
+    addresses = 3'b0; inputs = 7'bXXXXXXX0; #1000
+    $display("%b %b | %b | 0", addresses, inputs, out);
+  end
+endmodule
