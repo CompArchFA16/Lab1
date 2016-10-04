a0bb03d2c27ca7be3d8daa6b6b4e967d276c892f
lisahachmann <lisa.hachmann@gmail.com>
2016-10-03 21:45:17 -0400

    now it compiles

--- adder.v
+++ adder.v
@@ -19,15 +19,15 @@ module structuralFullAdder
     wire AxorB;
     wire AandB;
     wire AxorBandCin;
-    wire BorSub;
+    wire bos0;
 
     // calculate the sum
-    `XOR xorgate1(AxorB, a, BorSub);
+    `XOR xorgate1(AxorB, a,bos0);
     `XOR xorgate2(sum, AxorB, carryin);
     `XOR xorgate3( bos0, subtract, b);
 
     // calculate the carryout
-    `AND andgate1(AandB, a, BorSub);
+    `AND andgate1(AandB, a, bos0);
     `AND andgate2(AxorBandCin, AxorB, carryin);
     `OR orgate(carryout, AandB, AxorBandCin);
 
@@ -40,7 +40,8 @@ module FullAdderSub4bit
     //output overflow,  // True if the calculation resulted in an overflow
     input[3:0] a,     // First operand in 2's complement format
     input[3:0] b,      // Second operand in 2's complement format
-    input carryin      // Carry in to discern ADD from SUB functionality
+    input carryin,      // Carry in to discern ADD from SUB functionality
+    input subtract
 );
 
     // define wires for the carryouts between the different adders
@@ -49,10 +50,10 @@ module FullAdderSub4bit
     wire carryout2;
 
     // Create 4 different adders for each of the bits
-    structuralFullAdder adder0(sum[0], carryout0, a[0], b[0], carryin); // carryin=0 for first bit
-    structuralFullAdder adder1(sum[1], carryout1, a[1], b[1], carryout0); // carryin=0 for first bit
-    structuralFullAdder adder2(sum[2], carryout2, a[2], b[2], carryout1); // carryin=0 for first bit
-    structuralFullAdder adder3(sum[3], carryout, a[3], b[3], carryout2); // carryin=0 for first bit
+    structuralFullAdder adder0(sum[0], carryout0, a[0], b[0], carryin, subtract); // carryin=0 for first bit
+    structuralFullAdder adder1(sum[1], carryout1, a[1], b[1], carryout0, subtract); // carryin=0 for first bit
+    structuralFullAdder adder2(sum[2], carryout2, a[2], b[2], carryout1, subtract); // carryin=0 for first bit
+    structuralFullAdder adder3(sum[3], carryout, a[3], b[3], carryout2, subtract); // carryin=0 for first bit
 
 endmodule
 
@@ -66,7 +67,8 @@ module Adder32bit
         output [31:0] sum,
         input[31:0] a,
         input[31:0] b,
-        input carryin //Addition or Subtraction, also known as the first carryin
+        input carryin,
+        input subtract
     );
 
     wire carryout0;
@@ -78,16 +80,14 @@ module Adder32bit
     wire carryout6;
     wire carryout7;
 
-
-
-    FullAdderSub4bit adder0(sum[3:0], carryout0, a[3:0], b[3:0], carryin); // If carryin = 1 then it subtracts. first 4 bits
-    FullAdderSub4bit adder1(sum[7:4], carryout1, a[7:4], b[7:4], carryout0); //link carry, 2nd 4 bits
-    FullAdderSub4bit adder2(sum[11:8], carryout2, a[11:8], b[11:8], carryout1); //link carry, 3rd 4 bits
-    FullAdderSub4bit adder3(sum[15:12], carryout3, a[15:12], b[15:12], carryout2); //link carry, 4th 4 bits
-    FullAdderSub4bit adder4(sum[19:16], carryout4, a[19:16], b[19:16], carryout3); //link carry, 5th 4 bits
-    FullAdderSub4bit adder5(sum[23:20], carryout5, a[23:20], b[23:20], carryout4); //link carry, 6th 4 bits
-    FullAdderSub4bit adder6(sum[27:24], carryout6, a[27:24], b[27:24], carryout5); //link carry, 7th 4 bits
-    FullAdderSub4bit adder7(sum[31:28], carryout, a[31:28], b[31:28], carryout6); //link carry, 8th 4 bits
+    FullAdderSub4bit adder0(sum[3:0], carryout0, a[3:0], b[3:0], carryin, subtract); // If carryin = 1 then it subtracts. first 4 bits
+    FullAdderSub4bit adder1(sum[7:4], carryout1, a[7:4], b[7:4], carryout0, subtract); //link carry, 2nd 4 bits
+    FullAdderSub4bit adder2(sum[11:8], carryout2, a[11:8], b[11:8], carryout1, subtract); //link carry, 3rd 4 bits
+    FullAdderSub4bit adder3(sum[15:12], carryout3, a[15:12], b[15:12], carryout2, subtract); //link carry, 4th 4 bits
+    FullAdderSub4bit adder4(sum[19:16], carryout4, a[19:16], b[19:16], carryout3, subtract); //link carry, 5th 4 bits
+    FullAdderSub4bit adder5(sum[23:20], carryout5, a[23:20], b[23:20], carryout4, subtract); //link carry, 6th 4 bits
+    FullAdderSub4bit adder6(sum[27:24], carryout6, a[27:24], b[27:24], carryout5, subtract); //link carry, 7th 4 bits
+    FullAdderSub4bit adder7(sum[31:28], carryout, a[31:28], b[31:28], carryout6, subtract); //link carry, 8th 4 bits
 
     // figure out if overflow according to 2's complement
     // Overflow happens when the most significant bit of the sum
