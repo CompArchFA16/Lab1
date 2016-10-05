39c562d18ab89cc22058934aede96ab230699967
Zarin Bhuiyan <zarin.bhuiyan@students.olin.edu>
2016-10-04 20:42:13 -0400

    we think this might work now

--- alu_addsub.v
+++ alu_addsub.v
@@ -1,3 +1,4 @@
+`include "multiplexer_2_input.v"
 `define AND and #330
 `define OR or #320
 `define NOT not #10
@@ -12,22 +13,27 @@ module ALUAddSub
 
   input           operandA,
   input           operandB,
-  input           carryin
+  input           carryin,
+  input           ifsub
   //input[2:0]      command
 );
 
     wire  B_compl;
+    wire B;
     wire abSum;
     wire abXor;
     wire abCinXor;
 
-    `AND andgate(abSum, a, b); 
-    `XOR xorgate(abXor, a, b); 
+    `NOT notgate(B_compl, operandB);
+    Multiplexer2Input(B, ifsub, {operandB, B_compl})
+    `AND andgate(abSum, operandA, B); 
+    `XOR xorgate(abXor, operandA, B); 
     `AND andgate2(abCinXor, abXor, carryin); 
     `OR orgate(carryout, abSum, abCinXor); 
     `XOR xorgate2(sum, abXor, carryin);
 
     //subtraction
-    `NOT notgate(B_compl, operandB);
+
+
 
 endmodule
