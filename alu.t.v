cb80d44c5b7824afd63e5a1b79d21f177de8d46c
William Lu <william.a.lu@gmail.com>
2016-10-06 22:58:29 -0400

    started basic gate test cases

--- alu.t.v
+++ alu.t.v
@@ -40,10 +40,10 @@ module testALU();
     cmd=`CMD_AND;
 
     $display("a b | res | expected res"); 
-    a=0; b=0;
-    $display("ADD   | A: %b %b %b %b  B: %b %b %b %b", a[31:24], a[23:16], a[15:8], a[7:0], b[31:24], b[23:16], b[15:8], b[7:0]);
-    $display("  ACT | R: %b %b %b %b  C: %b  O: %b  Z: %b", res[31:24], res[23:16], res[15:8], res[7:0], cout, ofl, zero);
-    $display("  EXP | R: 00000000 00000000 00000000 00000000  C: 0  O: 0  Z: 1");
+    a=0; b=0; #2000;
+    $display("AND  | A: %b %b %b %b  B: %b %b %b %b", a[31:24], a[23:16], a[15:8], a[7:0], b[31:24], b[23:16], b[15:8], b[7:0]);
+    $display("  ACT | R: %b %b %b %b", res[31:24], res[23:16], res[15:8], res[7:0]);
+    $display("  EXP | R: 00000000 00000000 00000000 00000000");
 
     $display("--------------------------------------------------------------------------------------");
     $display("TESTS FOR NAND");
