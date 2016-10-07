bde99c53737ecc843926023bfedd15fd09f14bc4
Ian Paul <ian.paul@students.olin.edu>
2016-10-07 02:29:57 +0000

    added more test cases

--- 32bitGates.t.v
+++ 32bitGates.t.v
@@ -17,55 +17,55 @@ module test32bitGates ();
   xor32 testxor32(AxorB, A, B);
 
     initial begin
-    $display("                A                                      B                     |                   out      ");
+    $display("                A                                      B                 |                   out      ");
     // NAND
     A=4'b0000; B=4'b0000; #1000
     $display("NAND %b  %b  |  %b ", A, B, AnandB);
-    A=4'b1111; B=4'b1111; #1000
+    A=32'hffffffff; B=32'hffffffff; #1000
     $display("NAND %b  %b  |  %b ", A, B, AnandB);
-    A=4'b0000; B=4'b1111; #1000
+    A=4'b0000; B=32'hffffffff; #1000
     $display("NAND %b  %b  |  %b ", A, B, AnandB);
-    A=4'b0101; B=4'b1011; #1000
+    A=32'h55555555; B=32'h66666666; #1000
     $display("NAND %b  %b  |  %b ", A, B, AnandB);
     
     // NOR
     A=4'b0000; B=4'b0000; #1000
     $display("NOR  %b  %b  |  %b ", A, B, AnorB);
-    A=4'b1111; B=4'b1111; #1000
+    A=32'hffffffff; B=32'hffffffff; #1000
     $display("NOR  %b  %b  |  %b ", A, B, AnorB);
-    A=4'b0000; B=4'b1111; #1000
+    A=4'b0000; B=32'hffffffff; #1000
     $display("NOR  %b  %b  |  %b ", A, B, AnorB);
-    A=4'b0101; B=4'b1011; #1000
+    A=32'h55555555; B=32'h66666666; #1000
     $display("NOR  %b  %b  |  %b ", A, B, AnorB);
     
     // AND
     A=4'b0000; B=4'b0000; #1000
     $display("AND  %b  %b  |  %b ", A, B, AandB);
-    A=4'b1111; B=4'b1111; #1000
+    A=32'hffffffff; B=32'hffffffff; #1000
     $display("AND  %b  %b  |  %b ", A, B, AandB);
-		A=4'b0000; B=4'b1111; #1000
+	A=4'b0000; B=32'hffffffff; #1000
     $display("AND  %b  %b  |  %b ", A, B, AandB);
-    A=4'b0101; B=4'b1011; #1000
+    A=32'h55555555; B=32'h66666666; #1000
     $display("AND  %b  %b  |  %b ", A, B, AandB);
 
     // OR
     A=4'b0000; B=4'b0000; #1000
     $display("OR   %b  %b  |  %b ", A, B, AorB);
-    A=4'b1111; B=4'b1111; #1000
+    A=32'hffffffff; B=32'hffffffff; #1000
     $display("OR   %b  %b  |  %b ", A, B, AorB);
-    A=4'b0000; B=4'b1111; #1000
+    A=4'b0000; B=32'hffffffff; #1000
     $display("OR   %b  %b  |  %b ", A, B, AorB);
-    A=4'b0101; B=4'b1011; #1000
+    A=32'h55555555; B=32'h66666666; #1000
     $display("OR   %b  %b  |  %b ", A, B, AorB);
     
     // XOR
     A=4'b0000; B=4'b0000; #1000
     $display("XOR  %b  %b  |  %b ", A, B, AxorB);
-    A=4'b1111; B=4'b1111; #1000
+    A=32'hffffffff; B=32'hffffffff; #1000
     $display("XOR  %b  %b  |  %b ", A, B, AxorB);
-    A=4'b0000; B=4'b1111; #1000
+    A=4'b0000; B=32'hffffffff; #1000
     $display("XOR  %b  %b  |  %b ", A, B, AxorB);
-    A=4'b0101; B=4'b1011; #1000
+    A=32'h55555555; B=32'h66666666; #1000
     $display("XOR  %b  %b  |  %b ", A, B, AxorB);
     end
 
