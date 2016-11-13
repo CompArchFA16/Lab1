52b5951419870fad435f99386c63d81ace384083
yycho0108 <jchocholate@gmail.com>
2016-11-13 04:10:49 +0000

    finished test module

--- Makefile
+++ Makefile
@@ -3,5 +3,8 @@ all : alu.o
 alu.o : alu.t.v alu.v modules.v muxnbit.v
 	iverilog alu.t.v -o alu.o
 
+test : alu.o
+	./alu.o
+
 clean :
 	rm alu.o
--- alu.t.v
+++ alu.t.v
@@ -32,23 +32,28 @@ initial begin
 	$dumpfile("alu.vcd");
 	$dumpvars;
 
+	// =================================
 	// 4-bit exhaustive test cases
-	// Commented out due to incompatibility with 32-bit module
-	// Loop through all cases
+	// Commented out due to incompatibility with 32-bit module.
+	// Loops through all cases and shows output, though hard to interpret.
+	// =================================
 	//	for(operandA=-7; operandA<7; operandA=operandA+1) begin
 	//		for(operandB=-7; operandB<7; operandB=operandB+1) begin
-	//			for(command=0; command<1; command=command+1) begin
+	//			for(command=0; command<8; command=command+1) begin
 	//				#10000;
 	//				$display("%b %b %d | %b %b %b %b |", operandA, operandB, //command[2:0], result, carryout, zero, overflow);//
 	//			end//
 	//		end
 	//	end
+	// =================================
 
 
+	// =================================
 	// Worst case Propagation Delay
 	// Since there is no good way to determine the worst propagation delay,
 	// open alu.vcd with gtkwave and examine the time it takes for the final
 	// output to change values
+	// =================================
 	
 	// ADD
 	command = `c_ADD;
@@ -65,34 +70,39 @@ initial begin
 	operandA = -1; operandB = 0; #10000;	
 	operandA = -1; operandB = -1; #10000;
 
-	// 4 bit test Cases
-	command=0;
+	// =================================
+	// Starting Result Verifications Here
+	// =================================
+
+	command= `c_ADD;
 	$display("Testing ADD:");
-	`TEST(32'b1011,32'b1010);
-	`CHECK(32'b1011 + 32'b1010);
-	`TEST(32'b0110,32'b0100);
-	`CHECK(32'b0110 + 32'b0100);
-	`TEST(32'b1111,32'b1010);
-	`CHECK(32'b1111 + 32'b1010);
-	`TEST(32'b0001,32'b0101);
-	`CHECK(32'b0001 + 32'b0101);
-	`TEST(~(32'd0),1);
-	`CHECK(0);
+	$display(" A  B CMD | RES C_OUT ZERO OVERFLOW | PASS");
+	`TESTFLAGS(({4'b1011, 28'b0}),({4'b1010, 28'b0}),({4'b0101, 28'b0}),1'b1,1'b0,1'b1);
+	`TESTFLAGS(({4'b0110, 28'b0}),({4'b0100, 28'b0}),({4'b1010, 28'b0}),1'b0,1'b0,1'b1);
+	`TESTFLAGS(({4'b1111, 28'b0}),({4'b1010, 28'b0}),({4'b1001, 28'b0}), 1'b1, 1'b0, 1'b0);
+	`TESTFLAGS(({4'b0001, 28'b0}),({4'b0101, 28'b0}),({4'b0110, 28'b0}), 1'b0, 1'b0, 1'b0);
+
+	`TESTFLAGS(32'b1011,32'b1010, (32'b1011 + 32'b1010), 1'b0, 1'b0, 1'b0);
+	`TESTFLAGS(32'b0110,32'b0100, (32'b0110 + 32'b0100),1'b0, 1'b0, 1'b0);
+	`TESTFLAGS(32'b1111,32'b1010, (32'b1111 + 32'b1010),1'b0, 1'b0, 1'b0);
+	`TESTFLAGS(32'b0001,32'b0101, (32'b0001 + 32'b0101),1'b0, 1'b0, 1'b0);
+	`TESTFLAGS(~(32'd0),32'b1, 32'b0,1'b1,1'b1,1'b0);
 
 	// SUB Module Test
 	command= `c_SUB;
 	$display("Testing SUB:");
+	$display(" A  B CMD | RES C_OUT ZERO OVERFLOW | PASS");
 	//`TESTFLAGS(a, b, a-b, carryout, zero, overflow);
-	`TESTFLAGS(({4'b0010, 28'b0}),({4'b1010, 28'b0}),({4'b1000, 28'b0}),1'b0,1'b0,1'b1); // 2 - (-6)
-	`TESTFLAGS(({4'b1111, 28'b0}),({4'b1111, 28'b0}),32'b0,1'b1,1'b1,1'b0); // -1 - (-1)
+	`TESTFLAGS(({4'b0010, 28'b0}),({4'b1010, 28'b0}),({4'b1000, 28'b0}),1'b0,1'b0,1'b1);
+	`TESTFLAGS(({4'b1111, 28'b0}),({4'b1111, 28'b0}),32'b0,1'b1,1'b1,1'b0);
 	`TESTFLAGS(({4'b0100, 28'b0}),({4'b0010, 28'b0}),({4'b0010, 28'b0}), 1'b1, 1'b0, 1'b0);
 	`TESTFLAGS(({4'b0111, 28'b0}),({4'b1010, 28'b0}),({4'b1101, 28'b0}), 1'b0, 1'b0, 1'b1);
 
-	//`TESTCHECK(a, b, a-b);
-	`TESTCHECK(32'b0010,32'b1010,-32'd8);
-	`TESTCHECK(32'b1111,32'b1111,32'b0);
-	`TESTCHECK(32'b0100,32'b0010,32'b0010);
-	`TESTCHECK(32'b0111,32'b1010,-32'd3);
+	// Same test, now with 4 bits set on lower bits so no overflow should occur
+	`TESTFLAGS(32'b0010,32'b1010,-32'd8, 1'b0, 1'b0, 1'b0);
+	`TESTFLAGS(32'b1111,32'b1111,32'b0, 1'b1, 1'b1, 1'b0);
+	`TESTFLAGS(32'b0100,32'b0010,32'b0010, 1'b1, 1'b0, 1'b0);
+	`TESTFLAGS(32'b0111,32'b1010,-32'd3, 1'b0, 1'b0, 1'b0);
 
 
 	// SLT Module Test
