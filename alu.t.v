fe2250c5fa12ebada86918b34e2b3aaf43aa5c0f
yycho0108 <jchocholate@gmail.com>
2016-11-13 03:46:37 +0000

    fixing the sub test flags which were wrong

--- alu.t.v
+++ alu.t.v
@@ -3,11 +3,12 @@
 `include "cmd.v"
 `include "alu.v"
 
-`define DISP(exp) $display("%d & %d & %d & %d & %b &  %b  &  %b & %s", operandA, operandB, command, result, carryout, zero, overflow, ((result == exp)? "PASS" : "FAIL"))
-
 `define TEST(aValue,bValue) operandA= aValue; operandB  = bValue; #10000
+`define CHECK(exp) $display("%d & %d & %d & %d & %b &  %b  &  %b & %s", operandA, operandB, command, result, carryout, zero, overflow, ((result == exp)? "PASS" : "FAIL"))
+`define CHECKFLAGS(res,co,zr,of) `CHECK(res); flags = {result == res, carryout == co, zr == zero, overflow == of}; $display("%b, %s", flags, (flags == 4'b1111)? "PASS" : "FAIL")
 
-`define TESTCHECK(a,b,res) `TEST(a,b); `DISP(res)
+`define TESTCHECK(a,b,res) `TEST(a,b); `CHECK(res)
+`define TESTFLAGS(a,b,res, co, zr, of) `TEST(a,b); `CHECKFLAGS(res, co, zr, of)
 
 module alu_test();
 wire signed [31:0] result;
@@ -17,6 +18,7 @@ wire overflow;
 reg signed [31:0] operandA;
 reg signed [31:0] operandB;
 reg [31:0]      command;
+reg [3:0] flags;
 
 ALU #(.n(32)) alu(result,carryout,zero,overflow,operandA,operandB,command[2:0]);
 
@@ -27,6 +29,7 @@ initial begin
 	$dumpvars;
 
 	// 4-bit exhaustive test cases
+	// Commented out due to incompatibility with 32-bit module
 	// Loop through all cases
 	//	for(operandA=-7; operandA<7; operandA=operandA+1) begin
 	//		for(operandB=-7; operandB<7; operandB=operandB+1) begin
@@ -62,24 +65,24 @@ initial begin
 	command=0;
 	$display("Testing ADD:");
 	`TEST(32'b1011,32'b1010);
-	`DISP(32'b1011 + 32'b1010);
+	`CHECK(32'b1011 + 32'b1010);
 	`TEST(32'b0110,32'b0100);
-	`DISP(32'b0110 + 32'b0100);
+	`CHECK(32'b0110 + 32'b0100);
 	`TEST(32'b1111,32'b1010);
-	`DISP(32'b1111 + 32'b1010);
+	`CHECK(32'b1111 + 32'b1010);
 	`TEST(32'b0001,32'b0101);
-	`DISP(32'b0001 + 32'b0101);
+	`CHECK(32'b0001 + 32'b0101);
 	`TEST(~(32'd0),1);
-	`DISP(0);
+	`CHECK(0);
 
 	// SUB Module Test
-	command=1;
+	command= `c_SUB;
 	$display("Testing SUB:");
-	// //`TESTCHECK(a, b, a-b, zero, carryout, overflow);
-	// `TESTCHECK({4'b0010, 28'b0},{4'b1010, 28'b0},{4'b0111, 28'b0}, 0, 0, 0);
-	// `TESTCHECK({4'b1111, 28'b0},{4'b1111, 28'b0},{32'b0}, 1, 0, 0);
-	// `TESTCHECK({4'b0100, 28'b0},{4'b0010, 28'b0},{4'b0010, 28'b0}, 0, 1, 0);
-	// `TESTCHECK({4'b0111, 28'b0},{4'b1010, 28'b0},{4'b1101, 28'b0}, 0, 0, 1);
+	//`TESTFLAGS(a, b, a-b, carryout, zero, overflow);
+	`TESTFLAGS(({4'b0010, 28'b0}),({4'b1010, 28'b0}),({4'b1000, 28'b0}),1'b0,1'b0,1'b1); // 2 - (-6)
+	`TESTFLAGS(({4'b1111, 28'b0}),({4'b1111, 28'b0}),32'b0,1'b1,1'b1,1'b0); // -1 - (-1)
+	`TESTFLAGS(({4'b0100, 28'b0}),({4'b0010, 28'b0}),({4'b0010, 28'b0}), 1'b1, 1'b0, 1'b0);
+	`TESTFLAGS(({4'b0111, 28'b0}),({4'b1010, 28'b0}),({4'b1101, 28'b0}), 1'b0, 1'b0, 1'b1);
 
 	//`TESTCHECK(a, b, a-b);
 	`TESTCHECK(32'b0010,32'b1010,-32'd8);
@@ -89,7 +92,7 @@ initial begin
 
 
 	// SLT Module Test
-	command=2;
+	command=`c_SLT;
 	$display("Testing SLT:");
 	`TESTCHECK(32'd7,32'd2,32'd0);
 	`TESTCHECK(32'd1,32'd4,32'd1);
